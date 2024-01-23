d0a6146b3f18979bac2f7d56cc14fdc99235b609
Ayush Arora <ayusharora.282@gmail.com>
2024-01-23 11:06:00 +0530

    .

--- app.js
+++ app.js
@@ -4,18 +4,21 @@ console.log("hello");
 let button = document.getElementsByClassName("_2KpZ6l _2U9uOA _3v1-ww");
 if (button.length) {
   button[0].addEventListener("click", () => {
-    console.log("helllllllll");
+    console.log("helllllllllo");
     let elements = document.getElementsByClassName("B_NuCI");
     if (elements.length > 0) {
       let text = elements[0].innerText;
       console.log(text);
+      let image = document.getElementsByClassName("_396cs4 _2amPTt _3qGmMb");
+      let imageUrl = image[0].getAttribute("src");
+      let time = Date.now();
       chrome.runtime.sendMessage("", {
         type: "notification",
-        id: (Math.random() * 100).toString(),
+        id: time.toString(),
         options: {
           title: "A new item is added to your cart",
           message: `Item: ${text}`,
-          iconUrl: "./leaves-6975462_1280.webp",
+          iconUrl: imageUrl,
           type: "basic",
         },
       });
