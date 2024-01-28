73ad27fcd0fce961b102fccc609bdf562b7985bc
Archit <jainarchit1604@gmail.com>
2024-01-28 16:34:00 +0530

    Price Added

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import Button from "./components/Button.jsx";
 import Review from "./components/Api.jsx";
 import Cart from "./components/Cart.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Price from "./components/Price.jsx";
 
 export default class App extends Component {
   render() {
     return (
       <>
         <BrowserRouter>
-          <Navbar/>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route exact path="/Cart" element={<Cart />}></Route>
             <Route exact path="/Button" element={<Button />}></Route>
             <Route exact path="/Review" element={<Review />}></Route>
+            <Route exact path="/Price" element={<Price />}></Route>
             <Route path="/*" element={<Home />}></Route>
           </Routes>
         </BrowserRouter>
