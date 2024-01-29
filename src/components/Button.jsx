e52130d8a805d841763cdb6eeaccae3af2e40717
Archit <jainarchit1604@gmail.com>
2024-01-29 14:06:23 +0530

    changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,42 +1,67 @@
-/* eslint-disable no-undef */
+import React, { useEffect } from "react";
 
 import logo from "../assets/logo.jpg";
 
 const Popup = () => {
-  const openYouTube = () => {
-    const targetLink = "https://www.youtube.com/paid_memberships";
-    chrome.tabs.create({ url: targetLink });
-  };
+  useEffect(() => {
+    const openYouTube = () => {
+      const targetLink = "https://www.youtube.com/paid_memberships";
+      chrome.tabs.create({ url: targetLink });
+    };
 
-  const sendMessage = (action) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const msg = { action };
-      chrome.tabs.sendMessage(tabs[0].id, msg);
-    });
-  };
+    const sendMessage = (action) => {
+      console.log("hello");
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const msg = { action };
+      });
+      setTimeout(() => {
+        chrome.tabs.sendMessage(tabs[0].id, msg);
+      }, 1000);
+    };
 
-  const manageMembership = () => sendMessage("manageMembership");
-  const cancelMembership = () => sendMessage("cancelMembership");
-  const manageInGooglePlay = () => sendMessage("googleplay");
-  console.log(document.getElementById("openYouTubeButton"));
+    const manageMembership = () => sendMessage("manageMembership");
+    const cancelMembership = () => sendMessage("cancelMembership");
+    const manageInGooglePlay = () => sendMessage("googleplay");
+
+    document
+      .getElementById("openYouTubeButton")
+      .addEventListener("click", openYouTube);
+    document
+      .getElementById("manage_membership")
+      .addEventListener("click", manageMembership);
+    document
+      .getElementById("cancelButton")
+      .addEventListener("click", cancelMembership);
+    document
+      .getElementById("manageInGooglePlay")
+      .addEventListener("click", manageInGooglePlay);
+
+    return () => {
+      document
+        .getElementById("openYouTubeButton")
+        .addEventListener("click", openYouTube);
+
+      document
+        .getElementById("manage_membership")
+        .addEventListener("click", manageMembership);
+      document
+        .getElementById("cancelButton")
+        .addEventListener("click", cancelMembership);
+      document
+        .getElementById("manageInGooglePlay")
+        .addEventListener("click", manageInGooglePlay);
+    };
+  }, []);
 
   return (
     <div className="buttons">
       <div className="manipulation">
         <img src={logo} className="logo" alt="Ruse Reveal" />
         <h5>Directs the user to the membership page</h5>
-        <button id="openYouTubeButton" onClick={openYouTube}>
-          Membership Page
-        </button>
-        <button id="manage_membership" onClick={manageMembership}>
-          Manage Membership
-        </button>
-        <button id="cancelButton" onClick={cancelMembership}>
-          Cancel Membership
-        </button>
-        <button id="manageInGooglePlay" onClick={manageInGooglePlay}>
-          Manage in Google Play
-        </button>
+        <button id="openYouTubeButton">Membership Page</button>
+        <button id="manage_membership">Manage Membership</button>
+        <button id="cancelButton">Cancel Membership</button>
+        <button id="manageInGooglePlay">Manage in Google Play</button>
       </div>
     </div>
   );
